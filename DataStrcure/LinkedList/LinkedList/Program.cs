f0c724b9210e24184f66502799111216df6610f6
Pragyanshu Srivastava <pragyanshu21@gmail.com>
2025-07-09 23:16:43 +0530

    commiting all changes

---
+++ DataStrcure/LinkedList/LinkedList/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace LinkedList
+{
+    public class LinkedListProg
+    {
+        private Node head;
+        private Node tail;
+        private int size;
+
+        public LinkedListProg()
+        {
+            head = null;
+            tail = null;
+            size = 0;
+        }
+
+        public int length()
+        {
+            return size;
+        }
+
+        public bool isEmpty() 
+        { 
+            return size == 0;
+        }
+
+        public void addLast(int e)
+        {
+            Node newest = new Node(e, null);
+
+            if(this.isEmpty())
+            {
+                head = newest;
+            }
+            else
+            {
+                tail.Next = newest;
+            }
+
+            tail = newest;
+            size++;
+        }
+
+        public void display()
+        {
+            var p = head;
+            while (p != null)
+            {
+                Console.Write(p.Element + "-->");
+                p = p.Next;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            LinkedListProg linkedList = new LinkedListProg();
+            linkedList.addLast(99);
+            linkedList.addLast(50);
+            linkedList.addLast(99);
+            linkedList.display();
+            Console.ReadKey();
+        }
+    }
+}
